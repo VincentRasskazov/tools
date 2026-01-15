46e571b914f3a0936fd451d92a096779014c0ca4
havocgd <havocgdash@Gmail.com>
2026-01-15 21:34:19 +0000

    chore: stage and push all changes for full deploy

---
+++ .next/server/chunks/ssr/_next-internal_server_app_tools_calculator_page_actions_cb8c833e.js
@@ -0,0 +1,3 @@
+module.exports=[71606,(a,b,c)=>{}];
+
+//# sourceMappingURL=_next-internal_server_app_tools_calculator_page_actions_cb8c833e.js.map
---
+++ .next/server/middleware-build-manifest.js
@@ -0,0 +1,21 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/b9a0fabf54a78ef2.js",
+    "static/chunks/806bdb8e4a6a9b95.js",
+    "static/chunks/7a5c4cc87ad95c33.js",
+    "static/chunks/0afa5e999b6c353a.js",
+    "static/chunks/turbopack-087a0735679ab604.js"
+  ]
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
